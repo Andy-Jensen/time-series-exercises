b54b3508d439e0b9e72af9e10abf398f15329ec4
Andy Jensen <andrew.j.jensen91@gmail.com>
2023-02-06 15:44:03 -0600

    data acquisition exercises

---
+++ acquire.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+def get_people():
+    if os.path.isfile('people.csv'):
+        return pd.read_csv('people.csv')
+    else:
+        url='https://swapi.dev/api/people/'
+        response = requests.get(url)
+        data=response.json()
+        people=pd.DataFrame(data['results'])
+        df.to_csv('people.csv', index=False)
+        return df
+
+
+def get_planets():
+    if os.path.isfile('planets.csv'):
+        return pd.read_csv('planets.csv')
+    else:
+        url='https://swapi.dev/api/planets/'
+        response = requests.get(url)
+        data=response.json()
+        planets=pd.DataFrame(data['results'])
+        df.to_csv('planets.csv', index=False)
+        return df
+
+
+def get_starships():
+    if os.path.isfile('starships.csv'):
+        return pd.read_csv('starships.csv')
+    else:
+        url='https://swapi.dev/api/starships/'
+        response = requests.get(url)
+        data=response.json()
+        starships=pd.DataFrame(data['results'])
+        df.to_csv('starships.csv', index=False)
+        return df
+
+
+def get_starwars(x,y,z):
+    df=pd.concat([x, y, z], ignore_index=True)
+    return df
+
+
+def opsd():
+    if os.path.isfile('OPSD.csv'):
+        return pd.read_csv('OPSD.csv')
+    else:
+        df=pd.read_csv('https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv')
+        df.to_csv('OPSD.csv', index=False)
+        return df
